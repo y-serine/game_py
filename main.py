93a9402696be6037a98e3a890d7435066abf9d43
y-serine <serine.in.sericin@gmail.com>
2013-10-04 21:05:05 +0900

    changes

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ import codecs
 import time 
 import random 
 GRAB_MODE=False
+DISABLE_X_EXIT=False
+
+        
+class direction(object):
+    down="down"
+    up="up"
+    left="left"
+    right="right"
 
 
+
+#game body
 class Gamec(object):
+    #global ini
     def __init__(self):
         self.SCREEN_X = 640
         self.SCREEN_Y = 480
@@ -16,21 +27,23 @@ class Gamec(object):
         self.counter=0
         pygame.init()
         self.screen = pygame.display.set_mode(self.SCREEN_SIZE,pygame.DOUBLEBUF|pygame.HWSURFACE)
-        pygame.display.set_icon(pygame.image.load("serine.png").convert())
         pygame.display.set_caption("window")
+        pygame.display.set_icon(pygame.image.load("serine.png").convert())
         pygame.event.set_grab(GRAB_MODE)
         pygame.mixer.init()
-        pygame.mixer.music.set_volume(0.3)
-        
+
+    #run the game called by __call__
     def execute(self):
         while True:
             self.state()
             self.event.check()
-            
+
+    #exit the game   
     def end(self):
         pygame.display.quit()
         sys.exit()
-                    
+
+    #run the game           
     def __call__(self):
         self.execute()
 
@@ -38,30 +51,40 @@ class Gamec(object):
 class Statec(object):
     def __init__(self):
         self.state=self.first_ini
-        
+
+    #run the set function
     def __call__(self):
         self.state()
-        
+
+    #called once
     def first_ini(self):
+        pygame.mixer.music.set_volume(0.3)
         self.state=self.ini
-        
+
+    #called every time before the game starts
     def ini(self):
         self.state=self.play
         pygame.key.set_repeat(1,1)
-        
+
+    #temp main
     def play(self):
         game.draw()
-        
+
+    #called when you cleared the game
     def ending(self):
         pygame.key.set_repeat()
+
+
         
         
 class Drawc(object):
     def __init__(self):pass
-    
+
+    #called every frame
     def __call__(self):
         self.draw_main()
-        
+
+    #draw func main
     def draw_main(self):
         self.draw_HW()
         game.tools.syncfunc()
@@ -71,7 +94,7 @@ class Drawc(object):
     def draw_HW(self):
         game.screen.fill((100,100,100))
         font = pygame.font.Font(None, 36)
-        text = font.render("Hello World!", 1, (10, 10, 10))
+        text = font.render(u"Hello World! Press ESC to exit.", 1, (10, 10, 10))
         textpos = text.get_rect()
         textpos.centerx = game.screen.get_rect().centerx
         textpos.centery = game.screen.get_rect().centery-20
@@ -84,27 +107,31 @@ class Drawc(object):
 
         
 class Eventc(object):
-    def __init__(self):pass
+    def __init__(self):
+        pass
     
     def __call__(self):
         pass
-    
+
+    #checks the event list and passes them to the next
     def check(self):
         for event in pygame.event.get():
             if event.type == QUIT:
-                pass
-                #self.end()
+                if DISABLE_X_EXIT:
+                    pass
+                else:
+                    self.end()
             elif event.type==KEYDOWN:
                 if event.key == K_ESCAPE:
                     game.end()
                 elif event.key == K_q:
                     game.end()
                 elif event.key == K_RETURN:
-                    self.control.btn.M.push()
+                    game.control.btn.M.push()
                 elif event.key == K_z:
-                    self.control.btn.z.push()
+                    game.control.btn.z.push()
                 elif event.key == K_x:
-                    self.control.btn.x.push()
+                    game.control.btn.x.push()
 
                     
 class Controlc(object):
@@ -113,16 +140,19 @@ class Controlc(object):
         self.arr=self.Arrc()
         #self.reset()
         self.all=self.btn.all+self.arr.all
-        
+
+    #sets all zero
     def reset(self):
         self.btn.reset()
         self.arr.reset()
-        
+
+    #called every frame
     def process(self):
         self.btn.sync()
         self.arr.sync()
 
-        
+
+    #buttun objects
     class Btnc(object):
         def __init__(self):
             self.z=self.Btnname()
@@ -135,40 +165,48 @@ class Controlc(object):
             self.main=(self.z,self.x,self.a,self.s)
             self.sub=(self.M,self.S,self.C)
             self.all=self.main+self.sub
-            
+
+        #sets all zero
         def reset(self):
             for b in self.all:
                 b.reset()
-                
+
+        #called every frame in control
         def sync(self):
             for b in self.all:
                 b.sync()
-                
+
+        #if any buttun is pushed or not
         def pushed(self):
             return (self.z.pushed() or self.x.pushed() or self.a.pushed() or self.s.pushed())
 
         
         class Btnname(object):
             def __name__(self):
-                self.incl=0
+                self.inc=0
                 self.value=0
-                
+
+            #called when buttun is pushed 
             def push(self):
-                self.incl=1
+                self.inc=1
                 self.value += 1
-                
+
+            #sets all zero
             def reset(self):
-                self.incl=0
+                self.inc=0
                 self.value=0
-                
+
+            #called every frame
             def sync(self):
-                self.value*=self.incl
-                self.incl=0
-                
+                self.value*=self.inc
+                self.inc=0
+
+            #returns how long the buttun is pushed
             def pushed(self):
                 return self.value
 
-            
+
+    #arrow abjects
     class Arrc(object):
         def __init__(self):
             self.up=self.Arrnamec()
@@ -235,17 +273,122 @@ class Controlc(object):
                     self.reset()
                 self.incl=0
 
-            #if pushed    
+            #return how long pushed    
             def pushed(self):
                 return self.value
             
-        
+
 class Toolsc(object):
+    #methods called every frame
     def syncfunc(self):
         game.control.reset()
         game.counter+=1
         game.event.check()
         game.control.process()
+        
+    def make_path_picture(self,target):
+        return ("img/"+target)
+
+class Objectsc(object):
+    def __init__(self):
+        self.enemies=Enemiesc()
+    
+class Enemiesc(object):
+    def __init__(self):
+        self.charlist=[]
+        self.set_enemy_template()
+
+    #returns the list of enemies
+    def __call__(self):
+        return self.charlist
+
+    #adds a new enemy
+    def add(self,enemyname):
+        self.charlist.append(self.EnemyOne(enemyname))
+
+    def set_enemy_template(self):
+        self.name.temp_enemy=self.EnemyTemplatec("serine",10,"pattern")
+
+    class name(object):
+        pass
+
+    
+    #enemy template to make new enemies
+    class EnemyTemplatec(object):
+        def __init__(self,name,hp_max,pattern):
+            self.name=name
+            self.hp_max=hp_max
+            self.pattern=pattern
+            self.direction=direction.down
+            
+        def picture_load(self):
+            self.img=[]
+            for i in xrange(1,9):
+                img=self.name+"_"+str(i)+".png"
+                self.img.append(pygame.image.load(img).convert())
+                
+
+    #class called when making a new enemy
+    class EnemyOnec(EnemyTemplatec):
+        def __init__(self,enemyname,item):
+            self.enemyname=enemyname
+            self.pattern=enemyname.pattern
+            self.hp_max=enemyname.hp_max
+            self.item=item
+            self.hp=self.hp_max
+            
+        def killed(self):
+            game.objects.enemies.charlist.remove(self)
+            Effect.add("enemy_killed",5,1)
+            del self
+            
+        def check(self):
+            if self.hp<0:
+                self.killed()
+
+        
+class Effectsc(object):
+    def __init__(self):
+        self.effectlist=[]
+
+    #returns the list of enemies
+    def __call__(self):
+        return self.effectlist
+
+    #adds a new enemy
+    def add(self,effectname):
+        self.effectlist.append(self.EffectOne(effectname))
+
+        
+    #enemy template to make new enemies
+    class EffectTemplatec(object):
+        def __init__(self,name,count,picnum):
+            self.name=name
+            self.count=count
+            self.picnum=picnum
+            
+        def picture_load(self):
+            self.img=[]
+            for i in xrange(0,self.picnum-1):
+                img=self.name+"_"+str(i)+".png"
+                self.img.append(pygame.image.load(img).convert())
+    class name(object):
+        pass
+                
+
+    #class called when making a new enemy
+    class EffectOnec(EffectTemplatec):
+        def __init__(self,effectname):
+            self.effectname=effectname
+            self.count=effectname.count
+            
+        def sync(self):
+            self.count-=1
+            if self.count<0:
+                game.objects.effects.effectlist.remove(self)
+                del self
+
+                
 
     
 game=Gamec()
